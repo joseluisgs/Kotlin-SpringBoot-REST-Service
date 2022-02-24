ac091d394769a22e9ef895d85725e054580bcd61
José Luis González <joseluisgonsan@gmail.com>
2022-02-24 16:55:41 +0100

    get findbyid

--- src/main/kotlin/es/joseluisgs/kotlinspringbootrestservice/controllers/productos/CategoriasRestController.kt
+++ src/main/kotlin/es/joseluisgs/kotlinspringbootrestservice/controllers/productos/CategoriasRestController.kt
@@ -1,11 +1,13 @@
 package es.joseluisgs.kotlinspringbootrestservice.controllers.productos
 
 import es.joseluisgs.kotlinspringbootrestservice.config.APIConfig
+import es.joseluisgs.kotlinspringbootrestservice.errors.categorias.CategoriaNotFoundException
 import es.joseluisgs.kotlinspringbootrestservice.models.Categoria
 import es.joseluisgs.kotlinspringbootrestservice.repositories.CategoriasRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -20,7 +22,17 @@ class CategoriasRestController
 
     @GetMapping("")
     fun getAll(): ResponseEntity<MutableList<Categoria>> {
-        val res = categoriasRepository.findAll()
-        return ResponseEntity.ok(res)
+        val categorias = categoriasRepository.findAll()
+        return ResponseEntity.ok(categorias)
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): ResponseEntity<Categoria> {
+        try {
+            val categoria = categoriasRepository.findById(id).get()
+            return ResponseEntity.ok(categoria)
+        } catch (e: Exception) {
+            throw CategoriaNotFoundException(id)
+        }
     }
 }
--- src/main/kotlin/es/joseluisgs/kotlinspringbootrestservice/models/Cantegoria.kt
+++ src/main/kotlin/es/joseluisgs/kotlinspringbootrestservice/models/Cantegoria.kt
@@ -1,14 +1,12 @@
 package es.joseluisgs.kotlinspringbootrestservice.models
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 data class Categoria(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long,
+    @Column(unique = true)
     val nombre: String
 )
